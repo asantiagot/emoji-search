257da79cb4e58b5e8a5e5ccd42e7ae77b7eeaf7c
Antonio Santiago <asantiago@Antonios-MacBook-Pro.local>
2021-04-17 12:27:04 -0500

    Added base structure of EmojiBox, table/ tablebody, added emoji.json and added layout of 10 columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import { EmojiBox } from './components/EmojiBox/EmojiBox';
+import emojis from '../src/emojis.json';
 
 function App() {
   return (
     <div className="App">
-      <EmojiBox />
+      <EmojiBox emojis={emojis} />
     </div>
   );
 }
--- src/components/EmojiBox/EmojiBox.tsx
+++ src/components/EmojiBox/EmojiBox.tsx
@@ -1,11 +1,40 @@
 import styled from 'styled-components';
+import { Emoji } from '../../models';
+
+export interface EmojiBoxProps {
+    emojis: Emoji[];
+}
+
+interface TdProps {
+    index: number;
+}
 
 const Table = styled.table`
-    font-size: large;
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    /* grid-auto-rows: minmax(100px, auto); */
 `;
 
-export const EmojiBox: React.FC = () => {
+const TableBody = styled.tbody`
+    display: grid;
+`;
+
+const Td = styled.td<TdProps>`
+    grid-column: ${({ index }) => index % 10 + 1};
+`;
+
+const Searchbar = styled.input`
+`;
+
+export const EmojiBox: React.FC<EmojiBoxProps> = ({ emojis }) => {
     return (
-        <Table data-testid="emoji-box-table" />
+        <Table data-testid="emoji-box-table">
+            <thead>
+                <Searchbar />
+            </thead>
+            <TableBody>
+                {emojis && emojis.map((emoji: Emoji, index: number) => <Td key={index} index={index}>{emoji.character}</Td>)}
+            </TableBody>
+        </Table>
     );
 };
